b19848fa16d777eeec08e628335589dcf5f54612
Suraj009 <surajkumarmohapatra111@gmail.com>
2022-06-25 04:04:35 +0530

    html creation of registration page

--- registration-login-1/src/main/java/com/example/demo/service/web/UserRegController.java
+++ registration-login-1/src/main/java/com/example/demo/service/web/UserRegController.java
@@ -1,5 +1,26 @@
 package com.example.demo.service.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.example.demo.service.UserService;
+import com.example.demo.web.dto.UserRegDto;
+
+@Controller
+@RequestMapping("/registration")
+
 public class UserRegController {
+	private UserService userService;
+
+	public UserRegController(UserService userService) {
+		super();
+		this.userService = userService;
+	}
+
+	public String RegisterUserAccount(@ModelAttribute("user") UserRegDto regDto) {
+		userService.save(regDto);
+		return "redirected:/registration?success";
+	}
 
 }
